efd973db7317b698a3c48c62be0e0b815822efc6
Md : Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-06-29 09:01:49 +0600

    Add files via upload

---
+++ Data-Structure/Linked_List_DataStructure/Singly_Linked_List/Linked_List_delete.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct Node{
+int data;
+struct Node*next;
+};
+void print(struct Node*head){
+
+    while (head!=NULL)
+    {
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    cout<<"NULL"<<endl;
+}
+struct Node*vdelete(struct Node*head,int value){
+
+struct Node*current=(struct Node*)malloc(sizeof(struct Node));
+current->next=head;
+struct Node*temp=current;
+while (temp->next!=NULL)
+{
+    if(temp->next->data==value){
+        temp->next=temp->next->next;
+    }
+    temp=temp->next;
+}
+
+return current->next;
+
+}
+int main(){
+struct Node*a=NULL,*b=NULL,*c=NULL,*d=NULL;
+a=(struct Node*)malloc(sizeof(struct Node));
+b=(struct Node*)malloc(sizeof(struct Node));
+c=(struct Node*)malloc(sizeof(struct Node));
+d=(struct Node*)malloc(sizeof(struct Node));
+
+a->data=89;
+b->data=25;
+c->data=36;
+d->data=84;
+
+a->next=b;
+b->next=c;
+c->next=d;
+d->next=NULL;
+print(a);
+struct Node*newDelete;
+newDelete=vdelete(a,36);
+print(newDelete);
+return 0;
+}
